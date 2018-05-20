0073180814f95fa020032020db7cd6e551e0d6d4
DESKTOP-J2QSDC2\기루 <dlrfdnwkd@naver.com>
2018-05-20 22:34:07 +0900

    레이아웃 설정과 홈화면 수정

---
+++ OpenSouce/src/controller/Main.java
@@ -0,0 +1,55 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Smart °¡°èºÎ");
+		setRootLayout();
+		setHome();
+    }
+	
+	public void setRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setHome() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../view/Home.fxml"));
+			AnchorPane home = (AnchorPane) loader.load();
+			rootLayout.setCenter(home);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

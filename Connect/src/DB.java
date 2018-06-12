661b9f5c4879e90554a5e9e99c67ad2cfb5d6ff2
jej0605 <0j6e0j5@naver.com>
2018-06-12 17:19:10 +0900

    DB연동 url 수정, Driver 값 수정, 쿼리문 불러오기 추가

--- Connect/src/DB.java
+++ Connect/src/DB.java
@@ -2,17 +2,33 @@ import java.sql.*;
 
 public class DB {
 public static void main(String[] args) {
-	String url ="jdbc:mysql://localhost:3306/?useSSL=false&user=root&password=dmlwls159";
+	String url ="jdbc:mysql://127.0.0.1/?serverTimezone=UTC&verifyServerCertificate=false&useSSL=true&user=root&password=dmlwls159";
 	Connection conn=null;
 	Statement stmt=null;
 	ResultSet rs=null;
 	try {
-		Class.forName("com.mysql,jdbc.Driver");
-		System.out.println("드라이버 연결 성공");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("----------");
 		
 		conn = DriverManager.getConnection(url);
-		System.out.println("데이터베이스 접속 성공");
-	}
+		System.out.println("과소비판별");
+		
+		stmt =conn.createStatement();
+		String useopensourcedb = "use opensourcedb";
+		stmt.executeUpdate(useopensourcedb);
+		
+		String show과소비판별 = "select * from 과소비판별";
+		rs = stmt.executeQuery(show과소비판별);
+		while(rs.next()) {
+			System.out.printf("%-5s\t",rs.getString(1));
+			System.out.printf("%-5s\t",rs.getString(2));
+			System.out.printf("%-5s\t",rs.getString(3));
+			System.out.printf("%-5s\t",rs.getString(4));
+			System.out.printf("%-5s\t",rs.getString(5));
+			System.out.printf("%-5s\t",rs.getString(6));
+			
+		}
+		}
 	catch(ClassNotFoundException cnfe) {
 		cnfe.printStackTrace();
 	}

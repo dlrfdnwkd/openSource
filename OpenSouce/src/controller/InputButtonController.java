1a823c9593b2a3b9c87d4e488ae3d2d654fac2bb
DESKTOP-J2QSDC2\기루 <dlrfdnwkd@naver.com>
2018-06-12 09:40:29 +0900

    토글그룹 오류 해결과 선택불가 이벤트 수정(기초)

--- OpenSouce/src/controller/InputButtonController.java
+++ OpenSouce/src/controller/InputButtonController.java
@@ -1,22 +1,46 @@
 package controller;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class InputButtonController {
 private Stage dialogStage;
 private ObservableList<String> type = FXCollections.observableArrayList("교통","식비","생활","의료","유흥","기타");
 @FXML private ComboBox typeCheck; 
-	
+@FXML private RadioButton expenseButton;
+@FXML private RadioButton incomeButton;
+@FXML private RadioButton scheduleButton;
+@FXML private ToggleGroup group;
+@FXML private TextArea txtContend;
 	public void setDialogStage(Stage dialogStage) {
 		this.dialogStage = dialogStage;
 	}
 	@FXML 
 	public void initialize() {
 		typeCheck.setItems(type);
+		group.selectedToggleProperty().addListener((ObservableValue<? extends Toggle>observ, Toggle oldVal, Toggle newVal) -> {
+			String newStr = (String)newVal.getUserData();
+			if(newStr=="income") {
+			typeCheck.setDisable(true);
+			txtContend.setEditable(false);
+			}else {
+				if(newStr=="schedule") {
+					typeCheck.setDisable(false);
+					txtContend.setEditable(true);
+				}else {
+					
+				}
+			}
+		});
 	}
 	@FXML
 	public void confirmButton() {
